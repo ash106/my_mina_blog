cfc05185e81b2a19a319d1d83f36f5535d618b21
Alex Howington <alex.howington@gmail.com>
2013-09-30 03:32:35 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyMinaBlog::Application.config.secret_token = '0ddf13495a8524bfad0c833704e07b7fc4cee102b8ad9819982ba24b86b79f71402ff14cba65f3242cd4e27bdbb00c7d73a248e8653ebd7ad64cf1144e5708ca'
